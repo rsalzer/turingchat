5adf998ae6414a7ec2ced39a6bb37b91d40a72bd
Robert Salzer <robert.salzer@srf.ch>
2023-07-16 14:03:38 +0200

    test redis on server

--- app/api/increment/route.ts
+++ app/api/increment/route.ts
@@ -1,14 +1,26 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 
-export async function POST() {
-  const count = await redis.incr("counter");
+export async function POST(req: Request) {
+  const data = await req.json();
+  const hash: string = data.hash;
+  const keys: string[] = data.keys;
+  for await (const item of keys) {
+    await redis.hincrby(hash, item, 1);
+  }
+
+  const count = await redis.hgetall(hash);
   return NextResponse.json({ count });
 }
 
-export async function GET() {
-  const count = await redis.get("counter");
-  return NextResponse.json({ count });
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const hash = url.searchParams.get("hash");
+  if (hash != null) {
+    const count = await redis.hgetall(hash);
+    return NextResponse.json({ count });
+  }
+  return NextResponse.json({});
 }
