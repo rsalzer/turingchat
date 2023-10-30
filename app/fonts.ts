50728104bc90075bceb79805bcd55e028cc0f10c
Robert Salzer <robert.salzer@srf.ch>
2023-10-30 23:26:56 +0100

    remove fixture-font; remove middleware

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,4 +3,4 @@ import localFont from "next/font/local";
 // Font files can be colocated inside of `pages`
 export const boldFont = localFont({ src: "./TT Norms Pro Bold.ttf" });
 export const normalFont = localFont({ src: "./TT Norms Pro Regular.ttf" });
-export const headingFont = localFont({ src: "./Fixture-Black.otf" });
+export const headingFont = localFont({ src: "./TT Norms Pro Bold.ttf" });
