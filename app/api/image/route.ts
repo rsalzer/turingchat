4773515a32334e87f6a11eeb7f3145e66110b06a
Robert Salzer <robert.salzer@srf.ch>
2023-11-14 12:05:43 +0100

    add more logging

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -13,7 +13,7 @@ async function getData(prompt: string, id: number) {
     n: 1,
     size: "1024x1024",
   });
-  console.log("Image arrived from Openai");
+  console.log("Image arrived from Openai", id);
   const url = response.data[0].url;
   if (url && id !== 0)
     testUpload(url, id).then(() => console.log("Upload finished"));
