b9e75a0631e93aa9d187bfd596bb5e25baaad386
Robert Salzer <robert.salzer@srf.ch>
2023-11-14 12:11:13 +0100

    try on the edge (again...)

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
-import * as ftp from "basic-ftp";
-import get from "axios";
-import { Readable } from "stream";
+// import * as ftp from "basic-ftp";
+// import get from "axios";
+// import { Readable } from "stream";
+
+export const runtime = "edge";
 
 async function getData(prompt: string, id: number) {
   console.log("Start getting prompt", prompt);
@@ -14,38 +16,38 @@ async function getData(prompt: string, id: number) {
     size: "1024x1024",
   });
   console.log("Image arrived from Openai", id);
-  const url = response.data[0].url;
-  if (url && id !== 0)
-    testUpload(url, id).then(() => console.log("Upload finished"));
-  console.log("Not waiting to return ...");
+  // const url = response.data[0].url;
+  // if (url && id !== 0)
+  //   testUpload(url, id).then(() => console.log("Upload finished"));
+  // console.log("Not waiting to return ...");
   return response.data;
 }
 
-async function testUpload(url: string, id: number) {
-  const { data } = await get<Readable>(url, {
-    responseType: "stream",
-  });
-  const client = new ftp.Client();
-  client.ftp.verbose = false;
-  console.log("Start uploading file");
-  try {
-    const dateString = new Date()
-      .toISOString()
-      .replace(/T/, "_") // replace T with a space
-      .replace(/\..+/, "");
-    await client.access({
-      host: process.env.FTP_HOST,
-      user: process.env.FTP_USER,
-      password: process.env.FTP_PW,
-      secure: false,
-    });
-    await client.uploadFrom(data, `${id}/${dateString}.png`);
-  } catch (err) {
-    console.log(err);
-  }
-  client.close();
-  console.log("File uploading completed");
-}
+// async function testUpload(url: string, id: number) {
+//   const { data } = await get<Readable>(url, {
+//     responseType: "stream",
+//   });
+//   const client = new ftp.Client();
+//   client.ftp.verbose = false;
+//   console.log("Start uploading file");
+//   try {
+//     const dateString = new Date()
+//       .toISOString()
+//       .replace(/T/, "_") // replace T with a space
+//       .replace(/\..+/, "");
+//     await client.access({
+//       host: process.env.FTP_HOST,
+//       user: process.env.FTP_USER,
+//       password: process.env.FTP_PW,
+//       secure: false,
+//     });
+//     await client.uploadFrom(data, `${id}/${dateString}.png`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   client.close();
+//   console.log("File uploading completed");
+// }
 
 export async function POST(req: Request) {
   const data = await req.json();
