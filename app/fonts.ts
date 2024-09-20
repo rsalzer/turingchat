aa451135052f4bfa6dbfb25b91bfe2e16998e1f1
rsalzer <17248257+rsalzer@users.noreply.github.com>
2024-09-20 12:00:17 +0200

    use rubik instead of roboto

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,8 +1,8 @@
 import localFont from "next/font/local";
 
-import { Roboto } from "next/font/google";
+import { Rubik } from "next/font/google";
 
 // Font files can be colocated inside of `pages`
-export const boldFont = Roboto({ weight: "500", subsets: ["latin"] });
-export const normalFont = Roboto({ weight: "300", subsets: ["latin"] });
-export const headingFont = Roboto({ weight: "700", subsets: ["latin"] });
+export const boldFont = Rubik({ weight: "500", subsets: ["latin"] });
+export const normalFont = Rubik({ weight: "300", subsets: ["latin"] });
+export const headingFont = Rubik({ weight: "700", subsets: ["latin"] });
