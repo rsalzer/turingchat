4e3fccdd893ce7317343a25f3d9104a616aa8b07
rsalzer <17248257+rsalzer@users.noreply.github.com>
2024-09-20 11:51:18 +0200

    use roboto instead of TT Norms

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 
+import { Roboto } from "next/font/google";
+
 // Font files can be colocated inside of `pages`
-export const boldFont = localFont({ src: "./TT Norms Pro Bold.ttf" });
-export const normalFont = localFont({ src: "./TT Norms Pro Regular.ttf" });
-export const headingFont = localFont({ src: "./TT Norms Pro Bold.ttf" });
+export const boldFont = Roboto({ weight: "500", subsets: ["latin"] });
+export const normalFont = Roboto({ weight: "300", subsets: ["latin"] });
+export const headingFont = Roboto({ weight: "700", subsets: ["latin"] });
